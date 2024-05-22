2ef1ee525f58e15ee4506d8e70f35d76da37d923
myngann <160846244+myngann@users.noreply.github.com>
2024-05-22 19:09:52 +0700

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.mycompany.mavenprojecttest {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-    requires java.sql;
-
-    opens com.mycompany.mavenprojecttest to javafx.fxml;
-    exports com.mycompany.mavenprojecttest;
-}
